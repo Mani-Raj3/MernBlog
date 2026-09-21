2b399e9d03dbd0d32e7834de17ae9dd7cff6078d
Mani Raj <your-email@example.com>
2026-09-21 21:23:00 +0530

    view post count and read time 2 done

--- Backend/routes/Blog.js
+++ Backend/routes/Blog.js
@@ -1,20 +1,3 @@
-// import express from 'express'
-// import cors from "cors";
-// import { Create, Update, deletePost, getAllBlogs, getSingleBlog } from '../controllers/Blog.js'
-// //import { isAdmin } from '../middleware/isAdmin.js'
-// import  {isLogin}  from '../middleware/isLogin.js'
-
-// import { postUpload } from "../middleware/Multer.js";
-
-// const BlogsRoutes=express.Router()
-// BlogsRoutes.get("/", getAllBlogs);                  // Get all blogs
-// BlogsRoutes.get("/:id", getSingleBlog);             // Get one blog
-// BlogsRoutes.post('/create',isLogin,postUpload.single('postimage'),Create)
-// BlogsRoutes.patch('/update/:id',isLogin,postUpload.single('postimage'),Update)
-// BlogsRoutes.delete('/delete/:id',isLogin,deletePost)
-// export default BlogsRoutes
-
-
 import express from "express";
 
 import {
